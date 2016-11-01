4494f1eddaea8c6e988ec20f2ce609566047ae9a
Clementine Cottineau <clementine.cottineau@iscpif.fr>
2016-11-01 14:15:23 +0000

    include transitions in mapping function

--- AnalysisResultsViz.R
+++ AnalysisResultsViz.R
@@ -22,37 +22,65 @@ setwd("/Users/clementinecottineau/Documents/LandUseChangeLondon/data/")
 
 ukgrid = "+init=epsg:27700"
 latlong = "+init=epsg:4326"
+mappingVariables = c("PREVIOUS_USE", "CURRENT_USE", "PROPOSED_USE")
+years = c(2007, 2010, 2011)
+londonBoroughs = c("Barking and Dagenham", "Barnet", "Bexley", "Brent", "Bromley", "Camden", "City of London", 
+                   "Croydon", "Ealing", "Enfield", "Greenwich", "Hackney", "Hammersmith and Fulham", "Haringey",
+                   "Harrow", "Havering", "Hillingdon", "Hounslow", "Islington", "Kensington and Chelsea", "Kingston upon Thames",
+                   "Lambeth", "Lewisham", "Merton", "Newham", "Redbridge", "Richmond upon Thames", "Southwark", "Sutton", 
+                   "Tower Hamlets", "Waltham Forest", "Wandsworth", "Westminster")
 
-# import file
-nlud = read.csv("NLUD_ABCD_2007_LDN.csv", sep=",", dec=".")
+mapLandUseTransition = function(year, WhatToMap){
+  # import file
+  if(year == 2007) {
+    nlud = read.csv("NLUD_ABCD_2007_LDN.csv", sep=",", dec=".")
+    nlud$PREVIOUS_USE = nlud_SP_LL$PREVIOUS_LAND_USE
+  } else {
+    nlud = read.csv(paste0("NLUD_ABCD_", year, ".csv"), sep=",", dec=".", stringsAsFactors = F)
+     if(year %in% c(2011, 2012)) {
+      nlud$EASTING = as.numeric(nlud$Easting)
+      nlud$NORTHING = as.numeric(nlud$Northing)
+      nlud$PREVIOUS_USE = nlud$Previous.use
+      nlud$CURRENT_USE = nlud$Current.use
+      nlud$PROPOSED_USE = nlud$Proposed.use
+      nlud$AREA = as.numeric(nlud$Area)
+      nlud$PAO_DESCRIPTION = nlud$PAO.Description
 
-# transform data frame into spatialPointsDataFrame and reproject into ukgrid system
-nlud = subset(nlud, EASTING != "" | NORTHING != "" | !is.na(EASTING) | !is.na(NORTHING))
-nlud$nlud_ID <- 1:nrow(nlud)
-coords <- cbind( Easting = nlud$EASTING,Northing = nlud$NORTHING)
-nlud_SP <-SpatialPointsDataFrame(coords, data = data.frame(nlud), proj4string =CRS(ukgrid))
-nlud_SP_LL <- spTransform(nlud_SP,CRS(latlong))
-colnames (nlud_SP_LL@coords)[colnames(nlud_SP_LL@coords) == "Easting"] <- "Longitude"
-colnames(nlud_SP_LL@coords)[colnames (nlud_SP_LL@coords) == "Northing"] <- "Latitude"
-nlud_SP_LL@data$long = nlud_SP_LL@coords[,1]
-nlud_SP_LL@data$lat = nlud_SP_LL@coords[,2]
-
-# Interactive map
-nlud_SP_LL@data$varToMap = nlud_SP_LL@data$PREVIOUS_LAND_USE
-vals = unique(nlud_SP_LL@data$varToMap)
-n = length(vals)
-lookup = data.frame(plu = vals, plu_id = 1:n)
-nlud_SP_LL@data = data.frame(nlud_SP_LL@data, lookup[match(nlud_SP_LL@data$varToMap, lookup$plu),])
-factpal <- colorFactor(rainbow(n), nlud_SP_LL@data$plu_id)
-vLegendBox <- as.character(vals)
-leaflet(data = nlud_SP_LL) %>% addProviderTiles("CartoDB.Positron") %>%
-  clearShapes() %>% 
-  setView(lng=-0.1855676, lat=51.5371635, zoom=10) %>% 
-  addCircleMarkers(~long, ~lat, radius = ~ AREA, col=~factpal(plu_id) , 
-                   popup = ~paste(PAO_DESCRIPTION, " | Previously: ",PREVIOUS_LAND_USE, " | Proposed: ",PROPOSED_USE, sep=" "))
-
-
-
+            nlud = nlud[nlud$LA %in% londonBoroughs, ]
+     } else {
+       nlud = nlud[nlud$REGION == "London", ]
+     }
+    
+  }
+  
+  # transform data frame into spatialPointsDataFrame and reproject into ukgrid system
+  nlud = subset(nlud, EASTING != "" | NORTHING != "" | !is.na(EASTING) | !is.na(NORTHING))
+  nlud$nlud_ID <- 1:nrow(nlud)
+  coords <- as.data.frame(cbind( Easting = nlud$EASTING,Northing = nlud$NORTHING))
+  nlud_SP <-SpatialPointsDataFrame(coords, data = data.frame(nlud), proj4string =CRS(ukgrid))
+  nlud_SP_LL <- spTransform(nlud_SP,CRS(latlong))
+  colnames (nlud_SP_LL@coords)[colnames(nlud_SP_LL@coords) == "Easting"] <- "Longitude"
+  colnames(nlud_SP_LL@coords)[colnames (nlud_SP_LL@coords) == "Northing"] <- "Latitude"
+  nlud_SP_LL@data$long = nlud_SP_LL@coords[,1]
+  nlud_SP_LL@data$lat = nlud_SP_LL@coords[,2]
+  
+  # Interactive map
+  nlud_SP_LL@data$varToMap = nlud_SP_LL@data[, WhatToMap]
+  vals = unique(nlud_SP_LL@data$varToMap)
+  n = length(vals)
+  lookup = data.frame(plu = vals, plu_id = 1:n)
+  nlud_SP_LL@data = data.frame(nlud_SP_LL@data, lookup[match(nlud_SP_LL@data$varToMap, lookup$plu),])
+  factpal <- colorFactor(rainbow(n), nlud_SP_LL@data$plu_id)
+  vLegendBox <- as.character(vals)
+  summary(nlud_SP_LL@data)
+  leaflet(data = nlud_SP_LL) %>% addProviderTiles("CartoDB.Positron") %>%
+    clearShapes() %>% 
+    setView(lng=-0.1855676, lat=51.5371635, zoom=10) %>% 
+    addCircleMarkers(~long, ~lat, radius = ~AREA, col=~factpal(plu_id) , 
+                     popup = ~paste(PAO_DESCRIPTION, " | Previously: ",PREVIOUS_USE,
+                                    " | In ", year, ": ",CURRENT_USE, " | Proposed: ",PROPOSED_USE, sep=" "))
+}
+mapLandUseTransition(2007, mappingVariables[1])
 ########## transition 
 # transitions_m = table(nlud_SP_LL@data$PREVIOUS_LAND_USE, nlud_SP_LL@data$PROPOSED_USE)
 # transitions<-melt(transitions_m)
